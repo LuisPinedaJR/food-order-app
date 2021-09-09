b4dcf28a8548266e4ced157c104303bed0c9a69f
Luis Pineda <lspnd00@gmail.com>
2021-09-08 23:16:58 -0400

    added loading and error handling

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -7,12 +7,19 @@ import classes from './AvailableMeals.module.css'
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [httpError, setHttpError] = useState(null)
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
         'https://food-order-app-8d642-default-rtdb.firebaseio.com/meals.json'
       )
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!')
+      }
+
       const responseData = await response.json()
 
       const loadedMeals = []
@@ -26,9 +33,30 @@ const AvailableMeals = () => {
         })
       }
       setMeals(loadedMeals)
+      setIsLoading(false)
     }
-    fetchMeals()
+
+    fetchMeals().catch(error => {
+      setIsLoading(false)
+      setHttpError(error.message)
+    })
   }, [])
+
+  if (isLoading) {
+    return (
+      <section className={classes.mealsLoading}>
+        <p>Loading...</p>
+      </section>
+    )
+  }
+
+  if (httpError) {
+    return (
+      <section>
+        <p className={classes.mealsError}>{httpError}</p>
+      </section>
+    )
+  }
 
   const mealsList = meals.map(meal => (
     <MealItem
